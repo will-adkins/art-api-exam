bc822b359e3b9cadf381b8937930177ae0fe4b61
Will Adkins <wkadki01@gmail.com>
2018-06-25 11:12:04 -0400

    Post functionality completed

--- app.js
+++ app.js
@@ -4,13 +4,50 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const NodeHTTPError = require('node-http-error')
-const {} = require('ramda')
-const {} = require('./dal')
+
+const { not, propOr, isEmpty, pick } = require('ramda')
+
+const checkMissingKeys = require('./lib/check-missing-keys')
+const missingKeysMsg = require('./lib/missing-keys-msg')
+
+const { postPainting } = require('./dal')
 
 app.use(bodyParser.json())
 
 app.get('/', function(req, res, next) {
   res.send('Welcome to the Art API. Manage all the paintings.')
+})
+
+// Step 2: Create a Painting
+// name, movement, artist, yearCreated, and museum fields.
+app.post('/paintings', function(req, res, next) {
+  const newPainting = propOr({}, 'body', req)
+
+  if (isEmpty(newPainting)) {
+    next(
+      new NodeHTTPError(
+        400,
+        `You forgot to add a painting to the request body. Remember to use Content-Type: application/json, as that is the only format this api currently supports.`
+      )
+    )
+  }
+
+  const requiredKeys = ['name', 'movement', 'artist', 'yearCreated', 'museum']
+
+  if (not(isEmpty(checkMissingKeys(requiredKeys, newPainting)))) {
+    next(
+      new NodeHTTPError(
+        400,
+        missingKeysMsg(checkMissingKeys(requiredKeys, newPainting))
+      )
+    )
+  }
+
+  const cleanedPainting = pick(requiredKeys, newPainting)
+
+  postPainting(cleanedPainting)
+    .then(postResult => res.status(201).send(postResult))
+    .catch(err => next(new NodeHTTPError(err.status, err.message, err)))
 })
 
 app.use(function(err, req, res, next) {
@@ -21,6 +58,7 @@ app.use(function(err, req, res, next) {
       2
     )}`
   )
+  res.status(err.status || 500).send(err.message)
 })
 
 app.listen(port, () => console.log(`Success! Port: ${port}`))
--- dal.js
+++ dal.js
@@ -1,1 +1,19 @@
+require('dotenv').config()
+const { merge } = require('ramda')
+const pkGen = require('./lib/pkGen')
 
+const PouchDB = require('pouchdb-core')
+PouchDB.plugin(require('pouchdb-adapter-http'))
+PouchDB.plugin(require('pouchdb-find'))
+
+const db = new PouchDB(
+  `${process.env.COUCH_HOSTNAME}${process.env.COUCH_DBNAME}`
+)
+
+const postPainting = painting => {
+  const modifiedPainting = merge({ _id: pkGen(painting) }, painting)
+
+  return db.put(modifiedPainting)
+}
+
+module.exports = { postPainting }
