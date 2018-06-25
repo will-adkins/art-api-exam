13428d2df6952e6c0c381b90798557471cb6e91b
Will Adkins <wkadki01@gmail.com>
2018-06-25 10:21:40 -0400

    api up and running; error handling middleware and dependencies installed

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 require('dotenv').config()
+const port = process.env.PORT || 5000
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const NodeHTTPError = require('node-http-error')
+const {} = require('ramda')
+const {} = require('./dal')
+
+app.use(bodyParser.json())
 
 app.get('/', function(req, res, next) {
   res.send('Welcome to the Art API. Manage all the paintings.')
 })
+
+app.use(function(err, req, res, next) {
+  console.log(
+    `Error! \n\nMethod ${req.method} \nPath ${req.path} \n${JSON.stringify(
+      err,
+      null,
+      2
+    )}`
+  )
+})
+
+app.listen(port, () => console.log(`Success! Port: ${port}`))
